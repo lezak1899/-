b6d624b61d7bb8830c9d522b9098497ea359ce45
Lezak.Li <123456789Cc>
2023-08-20 11:25:10 +0800

    提交多线程的单元测试。

--- src/test/java/edu/lingnan/eattingwhat/EattingwhatApplicationTests.java
+++ src/test/java/edu/lingnan/eattingwhat/EattingwhatApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class eattingwhatApplicationTests {
+class EattingwhatApplicationTests {
 
     @Test
     void contextLoads() {
